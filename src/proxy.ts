e38e9f32e12c417dc8421e6ec15f66978e55d6cc
zidanLPTP <zidanlptp@gmail.com>
2025-12-06 22:22:25 +0700

    last fix vercel

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyToken } from "@/lib/auth"; // Helper yang kita buat di Batch 1
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // 1. Ambil path yang dituju
   const path = request.nextUrl.pathname;
 
